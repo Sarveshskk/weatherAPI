f2b83e5b7a78e78dbcacc3d857dfd8f7f94d788b
Sarveshskk <sarvesh@excellencetechnologies.in>
2022-04-01 15:44:11 +0530

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const route = require("./routes");
+const cors = require("cors")
 require("dotenv").config();
 
 const app = express();
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/weather",route.weatherRouter);
